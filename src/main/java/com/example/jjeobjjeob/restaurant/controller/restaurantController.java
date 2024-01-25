a8f85213f492638e6dfdc3ff7a5fd62edb0aa137
Jinyoung Jang <redpwon@gmail.com>
2024-01-25 13:47:46 +0900

    feat: restaurantController 생성

---
+++ src/main/java/com/example/jjeobjjeob/restaurant/controller/restaurantController.java
@@ -0,0 +1,9 @@
+package com.example.jjeobjjeob.restaurant.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class restaurantController {
+
+
+}
