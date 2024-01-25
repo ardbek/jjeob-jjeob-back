d61be6a24e1f271ef4cc0ea7f8b28aa6d9d42132
Jinyoung Jang <redpwon@gmail.com>
2024-01-25 13:43:03 +0900

    Initial commit

---
+++ src/main/java/com/example/jjeobjjeob/JjeobJjeobApplication.java
@@ -0,0 +1,13 @@
+package com.example.jjeobjjeob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjeobJjeobApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjeobJjeobApplication.class, args);
+	}
+
+}
