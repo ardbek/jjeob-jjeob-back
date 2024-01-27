ceb6f29f753733f8617ea836f3c641dc8385a672
hedgehog9 <redpwon@gmail.com>
2024-01-27 14:32:37 +0900

    feat: Restaurant Entity 추가

--- src/main/java/com/example/jjeobjjeob/restaurant/entity/Restaurant.java
+++ src/main/java/com/example/jjeobjjeob/restaurant/entity/Restaurant.java
@@ -3,8 +3,12 @@ package com.example.jjeobjjeob.restaurant.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Restaurant {
 
     // PK
