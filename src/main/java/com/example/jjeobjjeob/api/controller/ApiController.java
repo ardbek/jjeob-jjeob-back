ce4d9fc12731fd5b8f4e9054c055340578e27c1a
Jinyoung Jang <redpwon@gmail.com>
2024-01-29 17:56:10 +0900

    feat: api 호출 파라미터 추가

--- src/main/java/com/example/jjeobjjeob/api/controller/ApiController.java
+++ src/main/java/com/example/jjeobjjeob/api/controller/ApiController.java
@@ -1,11 +1,14 @@
 package com.example.jjeobjjeob.api.controller;
 
 import com.example.jjeobjjeob.api.dto.ApiRequest;
+import com.example.jjeobjjeob.api.dto.ApiResponse;
+import com.example.jjeobjjeob.api.dto.ApiResponseContainer;
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +17,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
+@Slf4j
 public class ApiController {
 
     @Value("${LOCALDATA_API_KEY}")
@@ -36,6 +42,13 @@ public class ApiController {
             apiUrl += "&state=" + apiReq.getState();
         }
 
+        // 음식점 코드
+        apiUrl += "&opnSvcId=07_24_04_P";
+        // 개발 : 최대 500건 / 운영 최대 10,000건
+        int pageSize = 500;
+        apiUrl += "&pageSize="+pageSize;
+
+
         URL url = new URL(apiUrl);
         BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
 
@@ -47,23 +60,66 @@ public class ApiController {
         }
         br.close();
 
+        // 웹에 출력 테스트 후 삭제 ---------------
         res.setCharacterEncoding("UTF-8");
         res.setContentType("application/json");
         res.getWriter().write(sb.toString());
+        // -------------------------------------
 
         Gson gson = new Gson();
         JsonObject jsonObject = JsonParser.parseString(sb.toString()).getAsJsonObject();
 
+        // totalCount 값 추출
         JsonObject result = jsonObject.getAsJsonObject("result");
         JsonObject header = result.getAsJsonObject("header");
         JsonObject paging = header.getAsJsonObject("paging");
-
-        // totalCount 값을 추출
         int totalCount = paging.get("totalCount").getAsInt();
 
-        System.out.println("Total Count: " + totalCount);
+        int totalPage = getTotalPage(totalCount, pageSize);
+
+        log.info("totalPage : {}",totalPage);
+
+        List<ApiResponse> responseList = new ArrayList<>();
+
+        for (int i = 0; i <= totalPage ; i++) {
+            String dataApiUrl = apiUrl + "&pageIndex=" + i;
+            url = new URL(dataApiUrl); // 수정된 URL 사용
+            br = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+
+            StringBuilder pageSb = new StringBuilder();
+            String pageTempStr;
+
+            while ((pageTempStr = br.readLine()) != null) {
+                pageSb.append(pageTempStr);
+            }
+
+            br.close();
+
+            ApiResponseContainer container = gson.fromJson(pageSb.toString(), ApiResponseContainer.class);
+            if (container != null && container.getRow() != null) {
+                responseList.addAll(container.getRow());
+            } else {
+                log.warn("No data found for page: {}", i);
+            }
+        }
+        log.info("Api Result :: {}",responseList);
 
 
+
+    }
+
+    /**
+     * 총 페이지 수
+     * @param totalCount
+     * @param pageSize
+     * @return
+     */
+    public int getTotalPage(int totalCount, int pageSize) {
+        if (totalCount % pageSize != 0) {
+            return (totalCount / pageSize) + 1;
+        } else {
+            return totalCount / pageSize;
+        }
     }
 
 }
---
+++ src/main/java/com/example/jjeobjjeob/api/dto/ApiResponseContainer.java
@@ -0,0 +1,12 @@
+package com.example.jjeobjjeob.api.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+public class ApiResponseContainer {
+    private List<ApiResponse> row;
+}
