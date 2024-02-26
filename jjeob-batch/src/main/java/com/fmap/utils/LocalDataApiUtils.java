007227123726aa342adec750c21e015f1f588a8f
Jinyoung Jang <redpwon@gmail.com>
2024-02-26 16:38:10 +0900

    refactor : ApiService 내용 Util로 분리

---
+++ jjeob-batch/src/main/java/com/fmap/utils/LocalDataApiUtils.java
@@ -0,0 +1,225 @@
+package com.fmap.utils;
+
+import com.fmap.dto.RestaurantReq;
+import com.fmap.dto.RestaurantRes;
+import com.fmap.restaurant.repository.RestaurantRepository;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Type;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class LocalDataApiUtils {
+
+    @Value("${LOCALDATA_API_KEY}")
+    private String LOCALDATA_API_KEY;
+    private final int PAGE_SIZE = 500;
+
+    private final RestaurantRepository restaurantRepository;
+
+    /**
+     * Api 데이터 호출
+     *
+     * @param apiUrl
+     * @return
+     * @throws Exception
+     */
+    public String getData(String apiUrl) throws Exception {
+        URL url = new URL(apiUrl);
+        BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream(), "UTF-8"));
+
+        StringBuilder sb = new StringBuilder();
+        String tempStr;
+
+        while ((tempStr = br.readLine()) != null) {
+            sb.append(tempStr);
+        }
+        br.close();
+
+        return sb.toString();
+    }
+
+
+    /**
+     * 데이터 파싱
+     *
+     * @param data
+     * @return
+     */
+    public List<RestaurantRes> parseStringToObject(String data) {
+        Gson gson = new Gson();
+        JsonObject jsonObject = JsonParser.parseString(data).getAsJsonObject();
+
+        JsonArray rows = jsonObject.getAsJsonObject("result")
+                .getAsJsonObject("body")
+                .getAsJsonArray("rows")
+                .get(0).getAsJsonObject()
+                .getAsJsonArray("row");
+
+        Type listType = new TypeToken<List<RestaurantRes>>() {
+        }.getType();
+        List<RestaurantRes> apiRespons = gson.fromJson(rows, listType);
+
+        return apiRespons;
+    }
+
+    /**
+     * 총 페이지 수 계산
+     *
+     * @param data
+     * @return
+     */
+    public int getTotalPage(String data) {
+        Gson gson = new Gson();
+        JsonObject jsonObject = JsonParser.parseString(data).getAsJsonObject();
+
+        // totalCount 값 추출
+        JsonObject result = jsonObject.getAsJsonObject("result");
+        JsonObject header = result.getAsJsonObject("header");
+        JsonObject paging = header.getAsJsonObject("paging");
+
+        int totalCount = paging.get("totalCount").getAsInt();
+        int totalPage = 0;
+
+        if (totalCount % PAGE_SIZE != 0) {
+            totalPage = (totalCount / PAGE_SIZE) + 1;
+        } else {
+            totalPage = totalCount / PAGE_SIZE;
+        }
+
+        log.info("ApiService :: totalPage = {}", totalPage);
+        return totalPage;
+    }
+
+    /**
+     * API 호출 및 데이터 업데이트
+     *
+     * @param apiReq
+     * @return
+     * @throws Exception
+     */
+    public List<RestaurantRes> fetchAndUpdateData(RestaurantReq apiReq) throws Exception {
+        String apiUrl = getApiURL(apiReq);
+        String data = getData(apiUrl);
+        int totalPage = getTotalPage(data);
+
+        List<RestaurantRes> updatedDataList = new ArrayList<>();
+        for (int i = 1; i <= totalPage; i++) {
+            String dataApiUrl = apiUrl + "&pageIndex=" + i;
+            String responseData = getData(dataApiUrl);
+            List<RestaurantRes> apiRespons = parseStringToObject(responseData);
+            if (apiRespons != null) {
+                updatedDataList.addAll(apiRespons);
+            }
+        }
+        return updatedDataList;
+    }
+
+    /**
+     * API 호출 및 데이터 업데이트
+     *
+     * @param
+     * @return
+     * @throws Exception
+     */
+    public List<RestaurantRes> fetchAndUpdateData(){
+
+        try {
+            String apiUrl = getApiURL();
+            String data = getData(apiUrl);
+            int totalPage = getTotalPage(data);
+
+            List<RestaurantRes> updatedDataList = new ArrayList<>();
+            for (int i = 1; i <= totalPage; i++) {
+                String dataApiUrl = apiUrl + "&pageIndex=" + i;
+                String responseData = getData(dataApiUrl);
+                List<RestaurantRes> apiRespons = parseStringToObject(responseData);
+                if (apiRespons != null) {
+                    updatedDataList.addAll(apiRespons);
+                }
+            }
+            return updatedDataList;
+        } catch (Exception e) {
+            log.error("LocalDataApiUtils :: fetchAndUpdateData() ====== ",e);
+            return null;
+        }
+    }
+
+
+    /**
+     * 요청 URL 생성
+     *
+     * @param apiReq
+     * @return
+     */
+    private String getApiURL(RestaurantReq apiReq) {
+        String url = "http://www.localdata.go.kr/platform/rest/TO0/openDataApi?authKey=" + LOCALDATA_API_KEY;
+
+        url += "&resultType=json"; // 반환 타입 default : xml
+        url += "&opnSvcId=07_24_04_P";// 음식점 코드
+        url += "&pageSize=" + PAGE_SIZE;// 개발 : 최대 500건 / 운영 최대 10,000건
+
+        if (apiReq.getLocalCode() != null) {
+            url += "&localCode=" + apiReq.getLocalCode();
+        }
+        if (apiReq.getBgnYmd() != null && apiReq.getEndYmd() != null) {
+            url += "&bgnYmd=" + apiReq.getBgnYmd() + "&endYmd=" + apiReq.getEndYmd();
+        }
+        if (apiReq.getState() != null) {
+            url += "&state=" + apiReq.getState();
+        }
+        if (apiReq.getState() != null) {
+            // 데이터 갱신일자 시작일
+            url += "&lastModTsBgn=" + apiReq.getLastModTsBgn();
+        }
+        if (apiReq.getState() != null) {
+            // 데이터 갱신일자 종료일
+            url += "&lastModTsEnd=" + apiReq.getLastModTsBgn();
+        }
+
+        return url;
+    }
+
+    private String getApiURL() {
+        String url = "http://www.localdata.go.kr/platform/rest/TO0/openDataApi?authKey=" + LOCALDATA_API_KEY;
+
+        url += "&resultType=json"; // 반환 타입 default : xml
+        url += "&opnSvcId=07_24_04_P";// 음식점 코드
+        url += "&pageSize=" + PAGE_SIZE;// 개발 : 최대 500건 / 운영 최대 10,000건
+
+        // TODO 나중에 현재 날짜로
+//        if (apiReq.getBgnYmd() != null && apiReq.getEndYmd() != null) {
+//            url += "&bgnYmd=" + apiReq.getBgnYmd() + "&endYmd=" + apiReq.getEndYmd();
+//        }
+//        if (apiReq.getState() != null) {
+//            url += "&state=" + apiReq.getState();
+//        }
+//        if (apiReq.getLastModTsBgn() != null) {
+//            // 데이터 갱신일자 시작일
+//            url += "&lastModTsBgn=" + apiReq.getLastModTsBgn();
+//        }
+//        if (apiReq.getLastModTsEnd() != null) {
+//            // 데이터 갱신일자 종료일
+//            url += "&lastModTsEnd=" + apiReq.getLastModTsBgn();
+//        }
+
+        return url;
+    }
+
+
+
+}
--- jjeob-batch/src/main/java/com/fmap/utils/LocationUtils.java
+++ jjeob-batch/src/main/java/com/fmap/utils/LocationUtils.java
@@ -6,16 +6,22 @@ import org.locationtech.proj4j.*;
 @Slf4j
 public class LocationUtils {
 
-    private final String EPSG_5174_PARAMS = "+proj=tmerc +lat_0=38 +lon_0=127.0028902777778 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43";
-    private final String WGS84CRS = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
+    private static final String EPSG_5174_PARAMS = "+proj=tmerc +lat_0=38 +lon_0=127.0028902777778 +k=1 +x_0=200000 +y_0=500000 +ellps=bessel +units=m +no_defs +towgs84=-115.80,474.99,674.11,1.16,-2.31,-1.63,6.43";
+    private static final String WGS84CRS = "+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs";
 
     /**
      * EPSG:5174 좌표계 값을 전달받아 WGS84 좌표계 값으로 반환
+     *
      * @param x
      * @param y
      * @return ProjCoordinate
      */
     public static ProjCoordinate CoordinateConversion(String x, String y) {
+
+        if (x == null || y == null || x.isEmpty() || y.isEmpty()) {
+            log.warn("Invalid latitude or longitude. Returning default coordinates.");
+            return null;
+        }
 
         double d_x = Double.parseDouble(x);
         double d_y = Double.parseDouble(y);
@@ -32,9 +38,8 @@ public class LocationUtils {
         ProjCoordinate wgs84Coord = new ProjCoordinate();
 
         transform.transform(tmCoord, wgs84Coord);
-        log.info("LocationUtils :: {},{}",wgs84Coord.x, wgs84Coord.y);
+        log.info("LocationUtils :: {},{}", wgs84Coord.x, wgs84Coord.y);
         return wgs84Coord;
     }
-
 
 }
