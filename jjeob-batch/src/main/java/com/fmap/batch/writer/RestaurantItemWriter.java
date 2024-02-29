939be3a1b32d0626748612192d4226132672d2d4
Jinyoung Jang <redpwon@gmail.com>
2024-02-29 16:15:34 +0900

    feat : 변경된 내용은 update 되도록 수정

--- jjeob-batch/src/main/java/com/fmap/batch/writer/RestaurantItemWriter.java
+++ jjeob-batch/src/main/java/com/fmap/batch/writer/RestaurantItemWriter.java
@@ -2,7 +2,9 @@ package com.fmap.batch.writer;
 
 import com.fmap.restaurant.entity.Restaurant;
 import com.fmap.restaurant.repository.RestaurantRepository;
+import com.fmap.utils.LocationUtils;
 import lombok.RequiredArgsConstructor;
+import org.locationtech.proj4j.ProjCoordinate;
 import org.springframework.batch.item.Chunk;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.stereotype.Component;
@@ -15,8 +17,50 @@ public class RestaurantItemWriter implements ItemWriter<Restaurant> {
 
     @Override
     public void write(Chunk<? extends Restaurant> chunk) throws Exception {
-        for (Restaurant restaurant : chunk) {
-            restaurantRepository.save(restaurant);
+        for (Restaurant item : chunk) {
+            Restaurant existingRestaurant = restaurantRepository.findByManagementNum(item.getManagementNum())
+                    .orElse(null);
+
+            if (existingRestaurant != null) {
+                updateExistingRestaurant(existingRestaurant, item);
+            } else {
+                restaurantRepository.save(item);
+            }
         }
     }
+
+    private void updateExistingRestaurant(Restaurant existingRestaurant, Restaurant newItem) {
+
+        String latitude = newItem.getEpsg5174X() != null ? newItem.getEpsg5174X().trim() : "";
+        String longitude = newItem.getEpsg5174y() != null ? newItem.getEpsg5174y().trim() : "";
+
+        ProjCoordinate wgs84Coord = null;
+        if (!latitude.isEmpty() && !longitude.isEmpty()) {
+            wgs84Coord = LocationUtils.CoordinateConversion(latitude, longitude);
+        }
+
+        String finalLatitude = (wgs84Coord != null) ? String.valueOf(wgs84Coord.x) : "";
+        String finalLongitude = (wgs84Coord != null) ? String.valueOf(wgs84Coord.y) : "";
+
+        existingRestaurant.builder().apiUpdateDt(newItem.getApiUpdateDt())
+                .bussSttus(newItem.getBussSttus())
+                .closureDt(newItem.getClosureDt())
+//                .bussType
+                .epsg5174X(newItem.getEpsg5174X())
+                .epsg5174y(newItem.getEpsg5174y())
+                .newAddr(newItem.getNewAddr())
+                .oldAddr(newItem.getOldAddr())
+                .latitude(finalLatitude)
+                .longitude(finalLongitude)
+                .newPostCode(newItem.getNewPostCode())
+                .oldPostCode(newItem.getOldPostCode())
+                .rstntNm(newItem.getRstntNm())
+                .opnSvcNm(newItem.getOpnSvcNm())
+                .managementNum(newItem.getManagementNum())
+                .build();
+
+        restaurantRepository.save(existingRestaurant);
+    }
+
+
 }
--- jjeob-core/src/main/java/com/fmap/restaurant/repository/RestaurantRepository.java
+++ jjeob-core/src/main/java/com/fmap/restaurant/repository/RestaurantRepository.java
@@ -4,9 +4,12 @@ import com.fmap.restaurant.entity.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface RestaurantRepository extends JpaRepository<Restaurant, Long> {
 
     List<Restaurant> findByRstntNm(String rstntNm);
+
+    Optional<Restaurant> findByManagementNum(String managementNum);
 
 }
