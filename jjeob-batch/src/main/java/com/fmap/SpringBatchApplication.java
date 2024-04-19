64cedc8be41cd5283ab9c0e1ce031ee23fe434dc
Jinyoung Jang <redpwon@gmail.com>
2024-04-19 15:18:16 +0900

    feat: war 파일로 생성되도록 수정, implement 중복 제거

--- jjeob-batch/src/main/java/com/fmap/SpringBatchApplication.java
+++ jjeob-batch/src/main/java/com/fmap/SpringBatchApplication.java
@@ -1,5 +1,6 @@
 package com.fmap;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableBatchProcessing
 @EnableScheduling
 @SpringBootApplication
+@Slf4j
 public class SpringBatchApplication implements CommandLineRunner {
 
     private JobLauncher jobLauncher;
@@ -25,6 +27,7 @@ public class SpringBatchApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBatchApplication.class, args);
+        log.debug("=== SpringBatchApplication run ===");
     }
 
     @Override
